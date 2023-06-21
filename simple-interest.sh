7039b66497e7b1b5999614080b31be537a422d67
Sienna Hsu <104809870+siennahsu@users.noreply.github.com>
2023-06-20 22:42:38 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# Sienna Hsu
+# siennahsu
 # Input:
 # p, principal amount
 # t, time period in years
